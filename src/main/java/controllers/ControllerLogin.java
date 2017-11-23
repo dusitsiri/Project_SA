d0f0e003fa0edabbb23f9e851f637998fae2fbb3
5810451187 <it_5601@hotmail.com>
2017-11-23 17:37:18 +0700

    Login finished

--- src/main/java/controllers/ControllerLogin.java
+++ src/main/java/controllers/ControllerLogin.java
@@ -1,16 +1,18 @@
 package controllers;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class ControllerLogin {
     @FXML private TextField passwordTextField;
@@ -21,13 +23,37 @@ public class ControllerLogin {
 
     public void initialize(){
         staffButton.setOnMouseClicked((MouseEvent e) -> {
-            showBlockInputPassword();
+            setOption();
             staffButton.setMnemonicParsing(true);
+            pipoButton.setMnemonicParsing(false);
         });
         pipoButton.setOnMouseClicked((MouseEvent e) -> {
-            showBlockInputPassword();
+            setOption();
             pipoButton.setMnemonicParsing(true);
+            staffButton.setMnemonicParsing(false);
+
         });
+
+        passwordTextField.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent event) {
+                if (event.getCode() == KeyCode.ENTER){
+                    TextField textField = (TextField) event.getSource();
+                    Stage stage = (Stage) textField.getScene().getWindow();
+                    try {
+                        checkUserAndPassword(stage);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+    public void setOption(){
+        passwordTextField.requestFocus();
+        showBlockInputPassword();
+        passwordTextField.setText("");
     }
 
     public void showBlockInputPassword(){
@@ -35,20 +61,43 @@ public class ControllerLogin {
         passwordTextField.setVisible(true);
     }
 
-    public void clickedLoginButton(ActionEvent event) throws IOException {
+    public void loginOnClick(ActionEvent event) throws IOException{
+        Button button = (Button) event.getSource();
+        Stage stage = (Stage) button.getScene().getWindow();
+        this.checkUserAndPassword(stage);
+    }
+
+    public void checkUserAndPassword(Stage stage) throws IOException {
         if (passwordTextField.getText().toLowerCase().equals("staff") && staffButton.isMnemonicParsing()){
-            loginComplete(event);
+            loginComplete(stage);
         }
         else if (passwordTextField.getText().toLowerCase().equals("pipo") && pipoButton.isMnemonicParsing()){
-            loginComplete(event);
+            loginComplete(stage);
+        }
+
+        else if (staffButton.isMnemonicParsing()&& passwordTextField.getText().toLowerCase().equals("")
+                || pipoButton.isMnemonicParsing() && passwordTextField.getText().toLowerCase().equals("")) {
+            Alert alert = new Alert(Alert.AlertType.WARNING, "กรุณากรอกรหัสผ่าน", ButtonType.CLOSE);
+            Optional optional = alert.showAndWait();
+        }
+
+        else if (staffButton.isMnemonicParsing() && !passwordTextField.getText().toLowerCase().equals("staff")
+                || pipoButton.isMnemonicParsing() && !passwordTextField.getText().toLowerCase().equals("pipo")) {
+            Alert alert = new Alert(Alert.AlertType.WARNING, "รหัสผ่านไม่ถูกต้อง", ButtonType.CLOSE);
+            Optional optional = alert.showAndWait();
+        }
+        else {
+            Alert alert = new Alert(Alert.AlertType.WARNING, "กรุณาเลือกหน่วยงาน", ButtonType.CLOSE);
+            Optional optional = alert.showAndWait();
         }
     }
 
-    public void loginComplete(ActionEvent event) throws IOException {
-        Button button = (Button) event.getSource();
-        Stage stage = (Stage) button.getScene().getWindow();
+    public void loginComplete(Stage stage) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("../portin.fxml"));
         stage.setScene(new Scene(loader.load()));
         stage.show();
     }
+
+
+
 }
