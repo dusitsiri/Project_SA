e626fef69f9fb006dd740cf3a9d2b0e39e6b5e5a
dusitsiri <dusit.si@ku.th>
2017-11-25 16:34:25 +0700

    Edit everythings

--- src/main/java/controllers/ControllerRecordPortOut.java
+++ src/main/java/controllers/ControllerRecordPortOut.java
@@ -3,20 +3,58 @@ package controllers;
 import database.DBMemberOfShip;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import models.MembersOfShip;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class ControllerRecordPortOut {
 
     DBMemberOfShip dbMemberOfShip = new DBMemberOfShip();
+    int countAddMembers = 0;
+    LocalDate birthdate;
+    @FXML private TableView<MembersOfShip> membersOfShipTableView;
+    @FXML private TextField nameTextField,positionTextField;
+    @FXML private RadioButton maleRadioBtn,femaleRadioBtn;
+    @FXML private DatePicker birthdayPicker;
 
-    @FXML private TableView membersOfShipTableView;
-    @FXML private TextField nameTextField,positionTextField,genderTextField,birthdayTextField;
+    public void initialize(){
+        birthdayPicker.setOnAction((ActionEvent event) ->{
+            birthdate = birthdayPicker.getValue();
+        });
 
-    public void clickAddMember(ActionEvent event){
+        System.out.println(birthdate);
+    }
+    public void clickAddMember(){
         int number = dbMemberOfShip.getCreateNumber();
-        int numberOfMembers = dbMemberOfShip.getCreateNumberOfPipoReport();
-        dbMemberOfShip.addMembersToDB(number, numberOfMembers, nameTextField.getText(),positionTextField.getText(),
-                genderTextField.getText(),birthdayTextField.getText());
+        int numberOfMembers = setNumberOfMembers(countAddMembers);
+        String name = nameTextField.getText();
+        String position =  positionTextField.getText();
+        String gender = checkGender();
+        String birthday = birthdate.toString();
+        dbMemberOfShip.addMembersToDB(number, numberOfMembers, name, position, gender, birthday);
+    }
+
+    public int setNumberOfMembers(int countAddMembers){
+        int numberOfMembers = 0;
+        if (countAddMembers == 0){
+            numberOfMembers = dbMemberOfShip.getCreateNumberOfPipoReport();
+            countAddMembers++;
+        }
+        else {
+            numberOfMembers = membersOfShipTableView.getSelectionModel().getSelectedItem().getNumberOfPipoReport();
+        }
+        return numberOfMembers;
+    }
+
+    public String checkGender(){
+        String gender = "";
+        if (maleRadioBtn.isSelected()) gender = "M";
+        else gender = "F";
+        return gender;
     }
 }
